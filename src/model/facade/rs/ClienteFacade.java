a8770f28ffbd6b6b5f38456b9a35adb2cb36b135
mariofonsecajni <mariofonsecajni@hotmail.com>
2016-03-02 20:56:50 -0300

    get

--- src/model/facade/rs/ClienteFacade.java
+++ src/model/facade/rs/ClienteFacade.java
@@ -3,6 +3,7 @@ package model.facade.rs;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -12,26 +13,24 @@ import javax.ws.rs.core.MediaType;
 import model.domain.Cliente;
 
 @Path("/cliente")
-@Consumes({MediaType.APPLICATION_JSON,
-		   MediaType.APPLICATION_XML})
-@Produces({MediaType.APPLICATION_JSON,
-	   MediaType.APPLICATION_XML})
+@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 public class ClienteFacade {
-	
+
 	@GET
 	public List<Cliente> getClientes() {
-		return Arrays.asList(new Cliente(1,"carlos","carlos@gmail.com"),
-							 new Cliente(2,"fulano","fulano@gmail.com"));
+		return Arrays.asList(new Cliente(1, "carlos", "carlos@gmail.com"),
+				new Cliente(2, "fulano", "fulano@gmail.com"));
 	}
-	
-	public List<Cliente> getClientes(Cliente cliente) {
-		if (cliente.getCodigo() == null) {
-			return Arrays.asList(new Cliente(0,"cliente não existe",""));
-		}
-		if (cliente.getCodigo() == 1) {
-			return Arrays.asList(new Cliente(1,"carlos","carlos@gmail.com"));
+
+	@GET
+	@Path("/{codigo}")
+	public List<Cliente> getClientes(@PathParam("codigo") Integer codigo) {
+
+		if (codigo == 1) {
+			return Arrays.asList(new Cliente(1, "carlos", "carlos@gmail.com"));
 		} else {
-			return Arrays.asList(new Cliente(3,"novo cliente","novo@gmail.com"));
+			return Arrays.asList(new Cliente(3, "novo cliente", "novo@gmail.com"));
 		}
 	}
 
