cc51dfbd02b03b9efb3ac6fdba199c64f1f6d55c
mariofonsecajni <mariofonsecajni@hotmail.com>
2016-03-02 21:48:57 -0300

    get sem list

--- src/model/facade/rs/ClienteFacade.java
+++ src/model/facade/rs/ClienteFacade.java
@@ -3,10 +3,11 @@ package model.facade.rs;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -25,13 +26,21 @@ public class ClienteFacade {
 
 	@GET
 	@Path("/{codigo}")
-	public List<Cliente> getClientes(@PathParam("codigo") Integer codigo) {
+	public Cliente getClientes(@PathParam("codigo") Integer codigo) {
 
 		if (codigo == 1) {
-			return Arrays.asList(new Cliente(1, "carlos", "carlos@gmail.com"));
+			return new Cliente(1, "carlos", "carlos@gmail.com");
 		} else {
-			return Arrays.asList(new Cliente(3, "novo cliente", "novo@gmail.com"));
+			return new Cliente(3, "novo cliente", "novo@gmail.com");
 		}
+	}
+	
+	@POST
+	public Cliente salvar(Cliente cliente){
+		
+		
+		return null;
+		
 	}
 
 }
